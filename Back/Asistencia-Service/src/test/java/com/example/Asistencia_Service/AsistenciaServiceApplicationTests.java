c2bc95b40d8fc9a724331280d0cb675d04cf879b
Darkrius <Piscontesoto@gmail.com>
2025-09-20 16:55:59 -0500

    Estructuracion del proyecto Back

---
+++ Back/Asistencia-Service/src/test/java/com/example/Asistencia_Service/AsistenciaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Asistencia_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
